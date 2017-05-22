a193040c721fd6af864628851d978b02c68ee621
Doug Rutnarak <dougrut1234@aol.com>
2017-05-22 17:39:24 -0400

    dumb stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "courses#index", as: "home"
   post "/courses/:course_id/questions/new" => "questions#create"
